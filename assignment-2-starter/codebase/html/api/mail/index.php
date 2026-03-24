c5ef0fa25481274e745203b2d4495bfbc32bcd20
Ibrahim325384 <ibkh010101@gmail.com>
2026-03-24 13:34:03 -0400

    RBAC added

--- assignment-2-starter/codebase/html/api/mail/index.php
+++ assignment-2-starter/codebase/html/api/mail/index.php
@@ -14,16 +14,38 @@ $mail = new Mail($database->getDb());
 $verifier = new Verifier();
 $verifier->decode($_SERVER['HTTP_AUTHORIZATION']);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$authentication = getallheaders();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($authentication['Authorization'])) {
     $data = json_decode(file_get_contents('php://input'), true);
     if (array_key_exists('name', $data) && array_key_exists('message', $data)) {
         $id = $mail->createMail($data['name'], $data['message']);
         $page->item(array("id" => $id));
+
+        if ($verifier->role == 'admin'){
+             $data['userID'] = $mail->getAllMail($id);
+             $data['userID'] = $mail->createMail($data['name'], $data['message']);
+        }
+        else if ($verifier->role == 'user'){
+            $verifier->userId = $mail->listMail();
+            $verifier->userId = $mail->createMail($data['name'], $data['message']);
+        }
+
     } else {
         $page->badRequest();
     }
-} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($authentication['Authorization'])) {
     $page->item($mail->listMail());
+
+    if ($verifier->role == 'admin'){
+             $data['userID'] = $mail->getAllMail($id);
+             $data['userID'] = $mail->createMail($data['name'], $data['message']);
+        }
+        else if ($verifier->role == 'user'){
+            $verifier->userId = $mail->listMail();
+            $verifier->userId = $mail->createMail($data['name'], $data['message']);
+        }
 } else {
     $page->badRequest();
+    http_response_code(401);
 }
